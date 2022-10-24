b9a156e38639d1a6b072c7e0beee9efee2eda9a1
Silvia Saverino <silviasave89@gmail.com>
2022-10-24 17:49:18 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Allows to save your data once you've done using this programm
+import pickle
+
+week = {'Monday: [] ,Tuesday: [], Wednesday: [], Thursday: [], Friday: [],Saturday:[], Sunday: []'}
+print(week)
