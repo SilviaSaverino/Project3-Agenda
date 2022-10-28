f685c14b6892fcc89e937eac205c1e6af8b51c0c
Silvia Saverino <silviasave89@gmail.com>
2022-10-28 22:07:29 +0000

    testing code for time of the day input

--- test.py
+++ test.py
@@ -18,6 +18,19 @@ def write_task():
         print(f'Sadly {day} is not a valid option:\n')
         write_task()
         time.sleep(1)
+    #tutto quello sotto deve andare in una funzione mi sa.
+    time = str(input('Any particular time of the day?\n'))
+    if time == MORNING:
+        print('You should do this between', MORNING)
+    elif time == AFTERNOON:
+        print('You should do this between', AFTERNOON)
+    elif time == EVENING:
+        print('You should do this between', EVENING)
+    elif time == NIGHT:
+        print('You should do this between', NIGHT)
+    else:
+        print('When again?')
+
 
     #chiamare funzione qui.
 
@@ -28,30 +41,34 @@ def write_task():
     #funzione per inserire dati nel file!
 
     
-
-    time = str(input('Any particular time of the day?').lower())
-    if time == Morning:
-        print('You should do this')
+  
     
 
-    
-    
+#add_task(day, task_detail)
 
-    add_task(day, task_detail)
-
-
-def add_task(day, task_detail):
-    new_pos_task = len(daysList[day - 1])
-    daysList[day - 1].append(new_pos_task, task_detail)
 
 
 write_task()
 
 
 def create_file():
+    """
+    Create a file.csv with the days in the months as header
+    """
     with open('YourAgenda.csv', 'w') as f:
+        header = [1,2,3,4,5,6,7,8,9,10,
+        11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
        
-create_file()    
+  
     
-def add_task_to_file()
-def edit_task_in_file() #rimuovere or edit?
+def add_task(day, task_detail):
+    """
+    Add the user task below the right header, 
+    the latter reprensenting the day of the month
+    previously chosen by the user.
+    """
+    new_pos_task = len(daysList[day - 1])
+    daysList[day - 1].append(new_pos_task, task_detail)
+
+
+#def edit_task_in_file() #rimuovere or edit?
